04ccac803f96fee5b819b7467076ede6c8931797
Khmels <Ser.khmel@gmail.com>
2021-07-04 12:18:11 +0300

    chapters 1

---
+++ src/chapter1/Program.java
@@ -0,0 +1,7 @@
+package chapter1;
+
+public class Program {
+    public static void main(String[] args) {
+        System.out.println("Hello Java once again!");
+    }
+}
---
+++ src/chapter2/Program.java
@@ -0,0 +1,13 @@
+package chapter2;
+
+public class Program {
+    //from site
+    public static void main (String args[]){
+        System.out.println("Hello Java!");
+    }
+
+    /*psvm idea
+    public static void main(String[] args) {
+    }
+    */
+}
